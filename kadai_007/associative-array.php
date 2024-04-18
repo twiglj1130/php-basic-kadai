99538c37e64defeb14cccfeebf9377514f320469
twiglj1130 <twig_lj_1130@yahoo.co.jp>
2024-04-18 21:31:58 +0900

    連想配列

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- http://no1.local/phptest/htdocs/kadai_007\associative-array.php -->
+</head>
+<body>
+    <?php
+    $personal_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    print_r($personal_data);
+    ?>
+</body>
+</html>
